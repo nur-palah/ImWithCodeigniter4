e190c13c357a34f45a9f4a3d187eeceba53b38d4
Nur Palah <corporation.np@gmail.com>
2022-06-21 07:23:45 +0700

    Add function to get selected id
    If you want to display data based on the id you choose.

--- using-model.php
+++ using-model.php
@@ -1,5 +1,6 @@
 <?php
 
+// IN YOUR MODEL / userModel.php
 namespace App\Models;
 
 use CodeIgniter\Model;
@@ -11,4 +12,43 @@ class userModel extends Model
     protected $allowedFields = [
         'name', 'email'
     ];
+    
+    // if you want to collect results based on the selected id
+    public function getId($id)
+    {
+        // if the id you are looking for does not exist then all will be displayed
+        if ($id == false) {
+            return $this->findAll();
+        }
+
+        // returns the first row in the result set
+        return $this->where(['id' => $id])->first();
+    }
+
 }
+
+// IN YOUR CONTROLLER crud.php
+
+namespace App\Controllers;
+
+use App\Models\userModel;
+
+class Crud extends BaseController
+{
+  protected $user;
+  
+  public function __construct()
+    {
+      $this->user = new userModel();
+    }
+
+  public function index($id)
+    {
+      $data = [
+        'dataUser' => $this->user->getId($id);
+      ];
+        
+      return view('/',$data);
+    }
+    
+?>
